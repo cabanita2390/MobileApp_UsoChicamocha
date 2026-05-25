48db0664657b3b3ce4ff3f3cd951ac302dfdb5e7
David Ortiz <judaortizchico@gmail.com>
2026-05-25 11:12:41 -0500

    Se resumen los cambios en el siguiente mensaje de commit:
    ```
    ajuste de compileSdk y targetSdk a la versión 35
    ```

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.example.testusoandroidstudio_1_usochicamocha"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.testusoandroidstudio_1_usochicamocha"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
