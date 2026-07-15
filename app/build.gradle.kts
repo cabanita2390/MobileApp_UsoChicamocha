f4b03477beaa23b34800fc594e3f35693e106b1b
David Ortiz <judaortizchico@gmail.com>
2026-07-15 11:37:54 -0500

    Usar signingConfig debugFixed en build de release
    Sin esto, el build de release no tenía firma configurada.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ android {
             //buildConfigField("String", "BASE_URL", "\"http://10.0.2.2:8080/api/\"")
         }
         release {
+            signingConfig = signingConfigs.getByName("debugFixed")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
